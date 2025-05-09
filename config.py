a23973b6365c3e5284edd4cb94c3fdcaffed2479
RVlad <reshetov664@gmail.com>
2025-05-09 08:31:50 +0700

    added config.py from .gitignore

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'hh_vacancies',
+    'user': 'postgres',
+    'password': '12345'
+}
+
+EMPLOYER_IDS = [9694561, 2282240, 3127, 2537115, 1809605, 370, 3388, 4181, 2748, 4719104]
